a8bf65d57d8a37eca8609aed02ccf10ad17e9ef8
HongpengM <mahongpengmars@163.com>
2018-07-31 16:49:50 +0800

    Initial commit

---
+++ init.el
@@ -0,0 +1,14 @@
+(require 'package)
+(package-initialize)
+
+;; Add more package resources
+(setq package-archives
+  '(("gnu" . "http://elpa.gnu.org/packages/")
+    ("marmalade" . "https://marmalade-repo.org/packages/")
+    ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+;;Set url proxy method
+(setq url-proxy-services
+      '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+	("http" . "127.0.0.1:1080")
+	("https" . "127.0.0.1:1080")))
