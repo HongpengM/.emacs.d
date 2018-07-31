02251c68e272b2979b0fb6c44802ea6fbeddf183
HongpengM <mahongpengmars@163.com>
2018-07-31 17:18:25 +0800

    elpy already added

--- init.el
+++ init.el
@@ -1,14 +1,15 @@
 (require 'package)
-(package-initialize)
+
 
 ;; Add more package resources
 (setq package-archives
   '(("gnu" . "http://elpa.gnu.org/packages/")
     ("marmalade" . "https://marmalade-repo.org/packages/")
     ("melpa" . "http://melpa.milkbox.net/packages/")))
-
+(package-initialize)
 ;;Set url proxy method
-(setq url-proxy-services
-      '(("no_proxy" . "^\\(localhost\\|10.*\\)")
-	("http" . "127.0.0.1:1080")
-	("https" . "127.0.0.1:1080")))
+;;(setq url-proxy-services
+;;      '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+;;	("http" . "127.0.0.1:1080")
+;;	("https" . "127.0.0.1:1080")))
+(elpy-enable)
