5e6b693011b1e7eb725e87e0ef8b5a37f42282da
HongpengM <mahongpengmars@163.com>
2018-07-31 18:04:24 +0800

    git add -AWeb mode install

--- init.el
+++ init.el
@@ -13,3 +13,39 @@
 ;;	("http" . "127.0.0.1:1080")
 ;;	("https" . "127.0.0.1:1080")))
 (elpy-enable)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (projectile web-mode elpy))))
+
+;; Web-mode options
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(setq web-mode-engines-alist '(("django" . "\\.html\\'")))
+
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+
+(setq web-mode-enable-auto-pairing t)
+(setq web-mode-enable-auto-expanding t)
+(setq web-mode-enable-css-colorization t)
+;; Projectile install
+;; Web-mode options                                                                                 
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(setq web-mode-engines-alist '(("django" . "\\.html\\'")))
+
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+
+(setq web-mode-enable-auto-pairing t)
+(setq web-mode-enable-auto-expanding t)
+(setq web-mode-enable-css-colorization t)
+
+(require 'projectile)
