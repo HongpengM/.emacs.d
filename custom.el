b69df96e28e952419e6a4909057baeb3f271067a
HongpengM <mahongpengmars@163.com>
2018-12-14 04:39:30 +0800

    fix: org-wiki cannot export

--- custom.el
+++ custom.el
@@ -66,7 +66,7 @@
  '(markdown-command "/usr/local/bin/macdown")
  '(package-selected-packages
    (quote
-    (smart-mode-line darktooth-theme gruvbox-theme kaolin-themes gotham-theme soothe-theme htmlize org-wiki nvm js-comint dockerfile-mode ensime paredit epm geiser julia-mode company-tern xref-js2 js2-refactor js2-mode markdown-mode magit projectile web-mode elpy)))
+    (## htmlize org-wiki nvm js-comint dockerfile-mode ensime paredit epm geiser julia-mode company-tern xref-js2 js2-refactor js2-mode markdown-mode magit projectile web-mode elpy)))
  '(safe-local-variable-values (quote ((encoding . utf-8)))))
 
 
