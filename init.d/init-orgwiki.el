656a3f828b5492c80e395f2915a426e01d318dda
HongpengM <mahongpengmars@163.com>
2018-12-14 21:16:32 +0800

    Revert "Revert "fix: org-wiki cannot export""
    This reverts commit c75633243e7bff13c2a44c3388c6fea4cc0bbc7c.

--- init.d/init-orgwiki.el
+++ init.d/init-orgwiki.el
@@ -5,31 +5,32 @@
 ;;==============================
 
 (require 'org-wiki)
+(req-package 
+  org-wiki 
+  :ensure t 
+  :force 
+  :init
+  ;;
+  ;; (setq org-wiki-location "~/org/wiki")
 
-;;
-;; (setq org-wiki-location "~/org/wiki")
+  (setq org-wiki-location-list '("~/org-wiki" ;; First wiki (root directory) is the default.
+				 ;;"~/Documents/wiki2 "
+				 ;;"~/Documents/wiki3"
+				 ))
 
-(setq org-wiki-location-list
-      '(
-        "~/org-wiki"    ;; First wiki (root directory) is the default.
-        ;;"~/Documents/wiki2 "
-        ;;"~/Documents/wiki3"
-        ))
+  ;; Initialize first org-wiki-directory or default org-wiki
+  (setq org-wiki-location (car org-wiki-location-list))
 
-;; Initialize first org-wiki-directory or default org-wiki
-(setq org-wiki-location (car org-wiki-location-list))
+  (setq org-wiki-default-read-only nil)	  ;; Default value
+  (setq org-wiki-server-port "8000")	  ;; 8000 - default value
+  (setq org-wiki-server-host "127.0.0.1") ;; Listen only localhost
 
-(setq org-wiki-default-read-only nil)  ;; Default value
-(setq org-wiki-server-port "8000") ;; 8000 - default value
-(setq org-wiki-server-host "127.0.0.1") ;; Listen only localhost
+  (add-to-list 'org-babel-load-languages '(js . t))
+  (org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages)
+  (add-to-list 'org-babel-tangle-lang-exts '("js" . "js"))
 
-(add-to-list 'org-babel-load-languages '(js . t))
-(org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages)
-(add-to-list 'org-babel-tangle-lang-exts '("js" . "js"))
-
-(setq org-wiki-template
-      (string-trim
-"
+  (setq org-wiki-template (string-trim
+			   "
 #+INCLUDE: theme/style.org
 #+TITLE: %n
 #+DESCRIPTION:
@@ -43,5 +44,7 @@
 
 * %n
 "))
+)
+
 
 (provide 'init-orgwiki)
